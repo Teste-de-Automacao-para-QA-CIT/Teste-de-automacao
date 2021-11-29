bd3d36cff378e6f345559b05256f74edd86fecb4
David-Jacobis <david.jacobis@outlook.com>
2021-11-29 09:32:47 -0300

    Alterações primeira parte

--- test/PrimeiraParte.java
+++ test/PrimeiraParte.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 /*
 
-CCenário Gherkin:
+Cenário Gherkin:
 
 Funcionalidade: Busca no Banco de Questões
 Cenário: Busca por questão inexistente
@@ -25,17 +25,17 @@ public class PrimeiraParte extends TestCase {
         String question = "Science: Computers"; // Variável para pesquisa 
 
         File file = new File("C:\\Users\\David\\Downloads\\chromedriver.exe"); //Coloque o caminho para o seu chromedrive
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()); //Especifica o driver de navegação
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()); //Especifica o driver de navegação (utilizado: Google Chrome)
         WebDriver driver = new ChromeDriver(); //Iniciando driver de navegação
         
         driver.get("https://opentdb.com/browse.php");// Link para busca 
-        driver.findElement(By.id("query")).sendKeys(question);//Selecionar a label de pesquisa pelo ID e preenche ela
+        driver.findElement(By.id("query")).sendKeys(question);//O driver irá selecionar a label de pesquisa pelo ID e preenche ela
         driver.findElement(By.xpath("//*[@id=\"page-top\"]/div[1]/form/div/button")).click();//Selecionar botão pelo caminho xpath
 
         String texto = driver.findElement(By.xpath("//*[@id=\"page-top\"]/div[2]/div")).getText(); // Pega o resultado da pesquisa e coloca em uma string
         assertEquals(texto, "No questions found."); // Compara o resultado obtido com o resultado esperado
         
-        driver.close();//Fechar o driver após a execução das atividades
+        driver.close();//Fechar o driver após a execução das atividades e teste realizado com sucesso
  
     }
 
